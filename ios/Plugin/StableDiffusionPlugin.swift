accae720d39b8dbc34512000d3dc0a42ef18abf3
himanushi <himanushi@users.noreply.github.com>
2023-02-27 20:58:05 +0900

    fix

--- ios/Plugin/StableDiffusionPlugin.swift
+++ ios/Plugin/StableDiffusionPlugin.swift
@@ -8,12 +8,11 @@ import CoreML
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
-@objc(CapCoreMLPlugin)
+@objc(StableDiffusionPlugin)
 @available(iOS 16.2, macOS 13.1, *)
 public class CapCoreMLPlugin: CAPPlugin, FileDownloaderDelegate {
     var downloader: FileDownloader?
-    let modelsDirName = "models"
-    var downloadProgress: Double { downloader!.progress || 0.0 }
+    var downloadProgress: Double { downloader!.progress }
 
     func downloadDidStart() {}
     func downloadDidFail(withError error: Error) { print("downloadDidFail", error) }
@@ -31,52 +30,46 @@ public class CapCoreMLPlugin: CAPPlugin, FileDownloaderDelegate {
     }
 
     @objc func download(_ call: CAPPluginCall) {
-        call.get
-        let url = "https://huggingface.co/coreml/coreml-stable-diffusion-2-1-base/resolve/main/split_einsum/stable-diffusion-v2.1-base_no-i2i_split-einsum.zip"
-
+        let url = call.getString("url")!
+        let modelsDirName = call.getString("modelsDirName")!
         downloader = FileDownloader(url: url, modelsDirName: modelsDirName)
         downloader!.delegate = self
         downloader!.startDownloading()
-        call.resolve([
-            "value": "queue"
-        ])
+        call.resolve()
     }
 
     @objc func unzip(_ call: CAPPluginCall) {
-        let url = "https://huggingface.co/coreml/coreml-stable-diffusion-2-1-base/resolve/main/split_einsum/stable-diffusion-v2.1-base_no-i2i_split-einsum.zip"
+        let url = call.getString("url")!
+        let modelsDirName = call.getString("modelsDirName")!
         downloader = FileDownloader(url: url, modelsDirName: modelsDirName)
         downloader!.delegate = self
         downloader!.unzip()
-        call.resolve([
-            "value": "unzip"
-        ])
+        call.resolve()
     }
 
     @objc func generateTextToImage(_ call: CAPPluginCall) {
+        call.resolve()
+        // "models/stable-diffusion-v2.1-base_split-einsum_compiled"
+        let resourcesAt = (Path.documents / call.getString("url")!).url
+        let prompt = call.getString("prompt")!
         do {
             let beginDate = Date()
             let pipelineConfig = MLModelConfiguration()
             pipelineConfig.allowLowPrecisionAccumulationOnGPU = true
             pipelineConfig.computeUnits = .cpuAndGPU
             pipelineConfig.preferredMetalDevice = .none
-            let pipeline = try StableDiffusionPipeline(resourcesAt: (Path.documents / "models/stable-diffusion-v2.1-base_split-einsum_compiled").url,
+            let pipeline = try StableDiffusionPipeline(resourcesAt: resourcesAt,
                                                        configuration: pipelineConfig,
                                                        reduceMemory: true)
-            print("Pipeline loaded in \(Date().timeIntervalSince(beginDate))")
             print("Generating...")
-            var configuration = StableDiffusionPipeline.Configuration(prompt: "a photo of car")
+            var configuration = StableDiffusionPipeline.Configuration(prompt: prompt)
             configuration.stepCount = 3
             configuration.schedulerType = .pndmScheduler
             configuration.guidanceScale = 3
-            let images = try pipeline.generateImages(configuration: configuration) {progress in
-                print(progress.step)
-                print(progress.step / progress.stepCount)
-                return true
-            }
+            let images = try pipeline.generateImages(configuration: configuration)
             let interval = Date().timeIntervalSince(beginDate)
-            print("Got images: \(images) in \(interval)")
             let image = images.compactMap({ $0 }).first
-            var imageStr = ""
+            var imageStr: String? = nil
             if let imageData = image {
                 let uiImage = UIImage(cgImage: imageData)
                 if let uiImageData = uiImage.jpegData(compressionQuality: 1.0) {
@@ -84,14 +77,8 @@ public class CapCoreMLPlugin: CAPPlugin, FileDownloaderDelegate {
                 }
             }
             print(imageStr)
-            call.resolve([
-                "value": imageStr
-            ])
         } catch {
             print(error)
-            call.resolve([
-                "value": "unloaded"
-            ])
         }
     }
 }
