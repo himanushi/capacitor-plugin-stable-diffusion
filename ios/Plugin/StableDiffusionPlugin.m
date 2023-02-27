89694e95047510e102cea194efbb9d40b468211a
himanushi <himanushi@users.noreply.github.com>
2023-02-27 17:32:07 +0900

    fix

--- ios/Plugin/StableDiffusionPlugin.m
+++ ios/Plugin/StableDiffusionPlugin.m
@@ -5,4 +5,7 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(StableDiffusionPlugin, "StableDiffusion",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(download, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(unzip, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(generateTextToImage, CAPPluginReturnPromise);
 )
